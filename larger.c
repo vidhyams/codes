750d412bab461a4a98b02f96603e2df27fa5e346
vidhyams <30457499+vidhyams@users.noreply.github.com>
2017-07-26 11:21:11 +0530

    Create larger.c

---
+++ larger.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+{
+ina a,b,c;
+printf(" enter the value of three numbers");
+scanf("%d%d%d",&a&b&c);
+if(a>b||a>c)
+printf(" a is large");
+elseif(b>a||b>c)
+printf("b is large");
+else("c is large")
+}
