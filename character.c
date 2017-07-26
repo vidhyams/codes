47c59aab2cc630c6e3b72c0d0c4dfc747962fe14
vidhyams <30457499+vidhyams@users.noreply.github.com>
2017-07-26 11:13:16 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+void main()
+{
+int c;
+printf("enter the character");
+scanf("%c",c);
+if(c==a-z||A-Z)
+printf(the value you entered is character");
+}
+else{
+printf("the value is not an character");
+}}
